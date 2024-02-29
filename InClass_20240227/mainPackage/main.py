7a430690f2032658a8a53dd38a9e1b30e9782a2f
mliso <mliso@DESKTOP-V75G00O.wifi.uc.edu>
2024-02-29 08:50:18 -0500

    This better work

--- InClass_20240227/mainPackage/main.py
+++ InClass_20240227/mainPackage/main.py
@@ -3,6 +3,7 @@
 # import the Vehicle class module
 from vehiclePackage.vehicleClass import *
 from vehiclePackage.hybridClass import Hybrid
+from vehiclePackage.engineClass import Engine
 
 if __name__ == "__main__":
     # Instantiate an object of type Vehicle
@@ -16,3 +17,6 @@ if __name__ == "__main__":
     # Instantiate an object of type Hybrid
     myPrius = Hybrid("Car", "Toyota", "Prius", 222)
     print(myPrius.printModel())
+    
+    myCar = Engine("Sports", 5.0)
+    print(myCar.printModel())
